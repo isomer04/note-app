a21540c5c93aebff4fb3cf531255483a79457d22
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-27 17:29:06 -0400

    Added a show action and view to display single note

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -57,7 +57,8 @@ class NoteController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $note = Note::where('id', $id)->where('user_id', Auth::id())->firstOrFail();
+        return view('notes.show')->with('note', $note);
     }
 
     /**
