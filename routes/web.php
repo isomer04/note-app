983c19f42e6ffddb27f49ecb2752c2325f08131a
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-26 21:57:03 -0400

    Created resource controller actions and Added an index method to fetch all notes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,6 +29,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Route::resource(['auth'])->name('dashboard');
+Route::resource('/notes', NoteController::class)->middleware(['auth']);
 
 require __DIR__.'/auth.php';
