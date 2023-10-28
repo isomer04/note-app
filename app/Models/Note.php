ed0706bdd003b00c163cb661dde11197ba00ce9d
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-28 17:26:04 -0400

    Defined relationships

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Note extends Model
 {
@@ -14,6 +14,12 @@ class Note extends Model
     public function getRouteKeyName(){
         return 'uuid';
     }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+
 
 
 }
