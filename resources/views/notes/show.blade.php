3657315fcf273677d5ef4d53934228f73af77688
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-29 22:38:45 -0400

    Added permanently delete models

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -29,7 +29,7 @@
                     <form action="{{ route('notes.destroy', $note) }}" method="post">
                         @method('delete')
                         @csrf
-                        <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure you wish to delete this note?')">Move to Trash</button>
+                        <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure you wish to move this to trash?')">Move to Trash</button>
                     
                     </form>
 
@@ -53,12 +53,12 @@
 
                     {{-- <a href="{{ route('notes.edit', $note) }}" class="btn-link ml-auto">Edit Note</a> --}}
 
-                    {{-- <form action="{{ route('notes.destroy', $note) }}" method="post">
+                    <form action="{{ route('trashed.destroy', $note) }}" method="post">
                         @method('delete')
                         @csrf
-                        <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure you wish to delete this note?')">Move to Trash</button>
+                        <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure you wish to delete this note forever? This action cannot be undone')">Delete Forever</button>
                     
-                    </form> --}}
+                    </form> 
                 @endif
 
             </div>
